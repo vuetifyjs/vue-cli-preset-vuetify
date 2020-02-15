834ee6638b874e1343f70c809ff24863da061317
John Leider <john.j.leider@gmail.com>
2020-02-15 01:12:56 -0500

    feat: improve preset, add support for installing plugins

--- generator.js
+++ generator.js
@@ -1,13 +1,59 @@
-module.exports = (api, opts) => {
-  api.onCreateComplete(() => {
-    const { exec } = require('child_process')
+// Utilities
+const chalk = require('chalk')
+const { exec } = require('child_process')
+const { upperFirst } = require('lodash')
+const {
+  log,
+  logWithSpinner,
+  stopSpinner,
+} = require("@vue/cli-shared-utils")
 
-    return exec(`vue add @vuetify/vue-cli-plugin-preset-${opts.preset}`, {
+async function run (api, command) {
+  let subprocess
+
+  try {
+    subprocess = exec(command, {
       cwd: api.resolve(),
       env: {
         ...process.env,
         VUE_CLI_SKIP_DIRTY_GIT_PROMPT: true,
-      },
-    }, err => err && console.log(err))
+      }
+    }, (err, stdout, stderr) => Promise.resolve({ stderr: err }))
+  } catch (e) { /* */ }
+
+  return subprocess
+}
+
+module.exports = (api, { preset = 'base' }) => {
+  // TODO: This should be in the base plugin.
+  // Ran into issue with duplicate extends
+  // and temporarily moved it into here
+  api.extendPackage({
+    'eslintConfig': { 'extends': 'vuetify' },
+  })
+
+  api.onCreateComplete(async () => {
+    const presetName = chalk.blue(`Vuetify ${upperFirst(preset)} Preset`)
+    const projectName = chalk.yellow(api.rootOptions.projectName)
+
+    logWithSpinner(`🦈`, `Preparing CLI Preset. This may take a while...`)
+
+    const {
+      stderr,
+      stdout
+    } = await run(api, `vue add @vuetify/preset-${preset}`)
+
+    if (!stdout) {
+      return console.error(`Unable to add preset ${preset} due to error: ${stderr}`)
+    }
+
+    stdout.on('close', async () => {
+      stopSpinner()
+
+      await run(api, './node_modules/.bin/vue-cli-service run lint')
+
+      log(`🍣  Successfully generated ${projectName} from the ${presetName}.`)
+      log()
+    })
   })
 }
